cd293a62ac79f5ffe90fde3de88b0a1fb66411db
badriv1 <vinit.badrike@medtronic.com>
2017-09-29 08:37:14 -0500

    first commit

---
+++ Code/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code
+{
+    public class Class1
+    {
+        public static int Add(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+
+        public static int Subtract(int number1, int number2)
+        {
+            return number1 - number2;
+        }
+    }
+}
---
+++ Test/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Addtest()
+        {
+            Assert.AreEqual(3, Code.Class1.Add(1, 2));
+        }
+
+        [TestMethod]
+        public void Subtracttest()
+        {
+            Assert.AreEqual(2, Code.Class1.Subtract(4, 2));
+        }
+
+    }
+}
